20e8f4d30aae6f14c7f561a55f1114f9d4b20fa1
nitish <nitish14920@gmail.com>
2021-12-26 12:16:12 +0530

    server created for socket.io

---
+++ Client/js/script.js
@@ -0,0 +1,5 @@
+const socket = io("http://localhost:8000");
+
+const form = document.getElementById("message_form");
+const message = document.getElementById("message_send");
+const container = document.getElementById("container");
